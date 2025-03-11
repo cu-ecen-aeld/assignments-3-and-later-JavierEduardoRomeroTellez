52684f9a4e7d42916503c2915ab899deec330116
Javier Eduardo Romero Tellez <romerotellezjavier@gmail.com>
2025-03-11 09:59:08 -0700

    Fix some things

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -16,7 +16,6 @@
 #define PORT "9000"
 
 #define BACKLOG 10
-
 #define MAXBUFFERSIZE 20000
 #define MAXREADSIZE 1000
 bool exit_program = false;
@@ -33,11 +32,12 @@ void sigchld_handler(int s)
 
 void *get_in_addr(struct sockaddr *sa)
 {
-	if(sa->sa_family == AF_INET){
-		return &(((struct sockaddr_in*)sa)->sin_addr);
+	if (sa->sa_family == AF_INET)
+	{
+		return &(((struct sockaddr_in *)sa)->sin_addr);
 	}
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
 int main(int argc, char *argv[])
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 	struct sockaddr_storage their_addr;
 	socklen_t sin_size;
 	struct sigaction sa;
-	int yes=1;
+	int yes = 1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
 	char buff[MAXBUFFERSIZE];
@@ -65,24 +65,29 @@ int main(int argc, char *argv[])
 
 	openlog(NULL, 0, LOG_USER);
 
-	if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+		return -1;
 	}
 
-	for(p = servinfo; p != NULL; p = p->ai_next) {
+	for (p = servinfo; p != NULL; p = p->ai_next)
+	{
 		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-						p->ai_protocol)) == -1 ){
-					perror("server: socket");
-					continue;
-				}
-
-		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
+							 p->ai_protocol)) == -1)
+		{
+			perror("server: socket");
+			continue;
 		}
 
-		if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+		{
+			perror("setsockopt");
+			exit(-1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+		{
 			close(sockfd);
 			perror("server: bind");
 			continue;
@@ -93,15 +98,16 @@ int main(int argc, char *argv[])
 
 	freeaddrinfo(servinfo);
 
-	if (p == NULL) {
+	if (p == NULL)
+	{
 		fprintf(stderr, "server: failed to bind\n");
-		exit(1);
+		exit(-1);
 	}
 
-	if(argc > 1)
+	if (argc > 1)
 	{
 		int pid = fork();
-		if(pid == -1)
+		if (pid == -1)
 		{
 			perror("fork");
 			exit(-1);
@@ -116,44 +122,52 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if(listen(sockfd, BACKLOG) == -1) {
+	if (listen(sockfd, BACKLOG) == -1)
+	{
 		perror("listen");
-		exit(1);
+		exit(-1);
 	}
 
 	sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	if (sigaction(SIGINT, &sa, NULL) == -1) {
+	if (sigaction((SIGINT), &sa, NULL) == -1)
+	{
 		perror("SIGINT");
-		exit(1);
+		exit(-1);
 	}
 
-	if (sigaction(SIGTERM, &sa, NULL) == -1) {
+	if (sigaction((SIGTERM), &sa, NULL) == -1)
+	{
 		perror("SIGTERM");
-		exit(1);
+		exit(-1);
 	}
 
 	printf("server: waiting for connections...\n");
 
-	while(!exit_program) {
+	while (!exit_program)
+	{
 		sin_size = sizeof their_addr;
 		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if(new_fd == -1){
+		if (new_fd == -1)
+		{
 			perror("accept");
 			continue;
 		}
 
 		inet_ntop(their_addr.ss_family,
-				get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+				  get_in_addr((struct sockaddr *)&their_addr),
+				  s, sizeof s);
 		syslog(LOG_INFO, "Connection from %s\n", s);
 		printf("server: got connection from %s\n", s);
-		
+
 		fptr = fopen("/var/tmp/aesdsocketdata", "a+");
 		perror("Open file");
 
-		while(true){
-			if((numbytes = recv(new_fd, &buff[rec_size], MAXREADSIZE-1, 0)) == -1){
+		while (true)
+		{
+			if ((numbytes = recv(new_fd, &buff[rec_size], MAXREADSIZE - 1, 0)) == -1)
+			{
 				perror("recv");
 				exit(1);
 			}
@@ -161,27 +175,28 @@ int main(int argc, char *argv[])
 			printf("Server: received %d bytes\n", numbytes);
 			rec_size = rec_size + (numbytes);
 
-			if(buff[rec_size-1] == '\n')
+			if (buff[rec_size - 1] == '\n')
 			{
 				fwrite(buff, sizeof(char), rec_size, fptr);
 				printf("Server: saving %d bytes \n", numbytes);
 				fflush(fptr);
-				memset(buff, 0 , sizeof(buff));
+				memset(buff, 0, sizeof(buff));
 				rec_size = 0;
 
 				fseek(fptr, 0, SEEK_SET);
-				while(true)
+
+				while (true)
 				{
 					read_b = fread(buff, sizeof(char), MAXREADSIZE, fptr);
 
-					if(read_b == 0)
+					if (read_b == 0)
 					{
 						perror("fread");
 						break;
 					}
 					else
 					{
-						if(send(new_fd, buff, read_b, 0) == -1)
+						if (send(new_fd, buff, read_b, 0) == -1)
 						{
 							perror("send");
 						}
@@ -195,7 +210,7 @@ int main(int argc, char *argv[])
 				fclose(fptr);
 				break;
 			}
-			else if(0 == numbytes)
+			else if (0 == numbytes)
 			{
 				syslog(LOG_INFO, "Closed connection from %s\n", s);
 				printf("Server: received %d bytes \n", numbytes);
@@ -207,9 +222,9 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
+
 	printf("Server: Shuting down \n");
 	close(sockfd);
 	close(new_fd);
 	return 0;
 }
-
