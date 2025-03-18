90b87843fb867ac2e1b38c5ef2a17e9f6259d4ec
Javier Eduardo Romero Tellez <romerotellezjavier@gmail.com>
2025-03-18 11:40:14 -0700

    Threads and timer

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,13 +12,26 @@
 #include <signal.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
 
 #define PORT "9000"
-
 #define BACKLOG 10
 #define MAXBUFFERSIZE 20000
 #define MAXREADSIZE 1000
+#define FILE_PATH "/var/tmp/aesdsocketdata"
 bool exit_program = false;
+
+pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t list_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct ThreadNode {
+	pthread_t thread;
+	int client_fd;
+	struct ThreadNode *next;
+} ThreadNode;
+
+ThreadNode *thread_list_head = NULL;
 
 void sigchld_handler(int s)
 {
@@ -27,7 +40,7 @@ void sigchld_handler(int s)
 	exit_program = true;
 
 	printf("\n Server: exiting on signal \n");
-	remove("/var/tmp/aesdsocketdata");
+	remove(FILE_PATH);
 }
 
 void *get_in_addr(struct sockaddr *sa)
@@ -40,28 +53,169 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
+void *handle_client(void *client_fd_ptr){
+	int new_fd = *(int *)client_fd_ptr;
+	free(client_fd_ptr);
+	char buff[MAXBUFFERSIZE];
+	struct sockaddr_storage their_addr;
+	socklen_t sin_size = sizeof their_addr;
+	getpeername(new_fd, (struct sockaddr *)&their_addr, &sin_size);
+	char s[INET6_ADDRSTRLEN];
+	inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+	syslog(LOG_INFO, "Connection from %s\n", s);
+	printf("Server: got connection from %s\n", s);
+	FILE *fptr;
+	int numbytes;
+	uint rec_size = 0;
+	size_t read_b = 0;
+
+	pthread_mutex_lock(&file_mutex);
+	fptr = fopen(FILE_PATH, "a+");
+	if(fptr == NULL) {
+		perror("Open file");
+		syslog(LOG_ERR, "Failed to open file: %s", strerror(errno));
+		close(new_fd);
+		pthread_mutex_unlock(&file_mutex);
+		pthread_exit(NULL);
+	}
+	pthread_mutex_unlock(&file_mutex);
+
+	while (true){
+		if ((numbytes = recv(new_fd, &buff[rec_size], MAXREADSIZE - 1, 0)) == -1) {
+			perror("recv");
+			syslog(LOG_ERR, "recv error: %s", strerror(errno));
+			break;
+		}
+		printf("Server: received %d bytes\n", numbytes);
+		rec_size += numbytes;
+
+		if (buff[rec_size - 1] == '\n'){
+			pthread_mutex_lock(&file_mutex);
+			fwrite(buff, sizeof(char), rec_size, fptr);
+			fflush(fptr);
+			printf("Server: saving %d bytes \n", rec_size);
+			memset(buff, 0, sizeof(buff));
+			rec_size = 0;
+
+			fseek(fptr, 0, SEEK_SET);
+			while (true)
+			{
+				read_b = fread(buff, sizeof(char), MAXREADSIZE, fptr);
+				if (read_b == 0){
+					break;
+				} else {
+					if (send(new_fd, buff, read_b,0) == -1) {
+						perror("send");
+						syslog(LOG_ERR, "send error: %s", strerror(errno));
+						break;
+					}
+					printf("Server: sending more bytes %d\n", (int)read_b);
+				}
+			}
+			pthread_mutex_unlock(&file_mutex);
+
+			syslog(LOG_INFO, "Closed connection from %s\n", s);
+			close(new_fd);
+			fclose(fptr);
+			break;
+		} else if (0 == numbytes) {
+			syslog(LOG_INFO, "Closed connection from %s\n", s);
+			printf("Server: receivd %d bytes \n", numbytes);
+			break;
+		} else {
+			printf("Server: received %d bytes, waiting for more \n", numbytes);
+		}
+	}
+	close(new_fd);
+	fclose(fptr);
+	pthread_exit(NULL);
+}
+
+void add_thread_node_list(pthread_t thread, int client_fd) {
+	ThreadNode *new_node = malloc(sizeof(ThreadNode));
+	if(new_node == NULL){
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	new_node->thread = thread;
+	new_node->client_fd = client_fd;
+	new_node->next = NULL;
+
+	pthread_mutex_lock(&list_mutex);
+	if(thread_list_head == NULL){
+		thread_list_head = new_node;
+	} else {
+		ThreadNode *current = thread_list_head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	pthread_mutex_unlock(&list_mutex);
+}
+
+void join_and_remove_thread() {
+	pthread_mutex_lock(&list_mutex);
+	ThreadNode *current = thread_list_head;
+	ThreadNode *prev = NULL;
+
+	while (current != NULL){
+		int result = pthread_join(current->thread, NULL);
+		if (result == 0){
+			ThreadNode *temp = current;
+			current = current->next;
+			if (prev) {
+				prev->next = current;
+			} else {
+				thread_list_head = current;
+			}
+			free(temp);
+		} else if(result == EDEADLK) {
+			prev = current;
+			current = current->next;
+		} else {
+			prev = current;
+			current = current->next;
+		}
+	}
+	pthread_mutex_unlock(&list_mutex);
+}
+
+void *timestamp_thread(void *arg){
+	FILE *fptr;
+	time_t rawtime;
+	struct tm *info;
+	char buffer[80];
+
+	while (!exit_program) {
+		pthread_mutex_lock(&file_mutex);
+		fptr = fopen(FILE_PATH, "a+");
+		if (fptr != NULL) {
+			time(&rawtime);
+			info = localtime(&rawtime);
+			strftime(buffer, sizeof(buffer), "timestamp:%a, %d %b %Y %H:%M:%S %z\n", info);
+			fprintf(fptr, "%s", buffer);
+			fclose(fptr);
+		}
+		pthread_mutex_unlock(&file_mutex);
+		sleep(10);
+	}
+	pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[])
 {
-	int sockfd, new_fd;
+	int sockfd;
 	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr;
-	socklen_t sin_size;
 	struct sigaction sa;
 	int yes = 1;
-	char s[INET6_ADDRSTRLEN];
 	int rv;
-	char buff[MAXBUFFERSIZE];
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
-	int numbytes;
-	uint rec_size = 0;
-	size_t read_b = 0;
-	FILE *fptr;
-
-	fprintf(stderr, "Server: argc = %d\n", argc);
 
 	openlog(NULL, 0, LOG_USER);
 
@@ -104,7 +258,7 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	if (argc > 1)
+	if (argc > 1 && strcmp(argv[1], "-d") == 0)
 	{
 		int pid = fork();
 		if (pid == -1)
@@ -145,86 +299,51 @@ int main(int argc, char *argv[])
 
 	printf("server: waiting for connections...\n");
 
+	pthread_t timestamp_thead_id;
+	if(pthread_create(&timestamp_thead_id, NULL, timestamp_thread, NULL) != 0) {
+		perror("pthread_create timestamp thread");
+		exit(-1);
+	}
+
 	while (!exit_program)
 	{
-		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1)
-		{
+		struct sockaddr_storage their_addr;
+		socklen_t sin_size = sizeof their_addr;
+		int *new_fd_ptr = malloc(sizeof(int));
+		*new_fd_ptr = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+
+		if(*new_fd_ptr == -1){
 			perror("accept");
+			free(new_fd_ptr);
 			continue;
 		}
 
-		inet_ntop(their_addr.ss_family,
-				  get_in_addr((struct sockaddr *)&their_addr),
-				  s, sizeof s);
-		syslog(LOG_INFO, "Connection from %s\n", s);
-		printf("server: got connection from %s\n", s);
-
-		fptr = fopen("/var/tmp/aesdsocketdata", "a+");
-		perror("Open file");
-
-		while (true)
-		{
-			if ((numbytes = recv(new_fd, &buff[rec_size], MAXREADSIZE - 1, 0)) == -1)
-			{
-				perror("recv");
-				exit(1);
-			}
-
-			printf("Server: received %d bytes\n", numbytes);
-			rec_size = rec_size + (numbytes);
-
-			if (buff[rec_size - 1] == '\n')
-			{
-				fwrite(buff, sizeof(char), rec_size, fptr);
-				printf("Server: saving %d bytes \n", numbytes);
-				fflush(fptr);
-				memset(buff, 0, sizeof(buff));
-				rec_size = 0;
-
-				fseek(fptr, 0, SEEK_SET);
-
-				while (true)
-				{
-					read_b = fread(buff, sizeof(char), MAXREADSIZE, fptr);
-
-					if (read_b == 0)
-					{
-						perror("fread");
-						break;
-					}
-					else
-					{
-						if (send(new_fd, buff, read_b, 0) == -1)
-						{
-							perror("send");
-						}
-						printf("Server: sending more bytes %d\n", (int)read_b);
-					}
-				}
-
-				syslog(LOG_INFO, "Closed connection from %s\n", s);
-
-				close(new_fd);
-				fclose(fptr);
-				break;
-			}
-			else if (0 == numbytes)
-			{
-				syslog(LOG_INFO, "Closed connection from %s\n", s);
-				printf("Server: received %d bytes \n", numbytes);
-				break;
-			}
-			else
-			{
-				printf("Server: received %d bytes, waiting for more \n", numbytes);
-			}
+		pthread_t client_thread;
+		if (pthread_create(&client_thread, NULL, handle_client, (void *)new_fd_ptr) != 0){
+			perror("pthread_create");
+			close(*new_fd_ptr);
+			free(new_fd_ptr);
+			continue;
 		}
+		add_thread_node_list(client_thread, *new_fd_ptr);
+		join_and_remove_thread();
 	}
 
 	printf("Server: Shuting down \n");
 	close(sockfd);
-	close(new_fd);
+
+	pthread_join(timestamp_thead_id, NULL);
+
+	while(thread_list_head){
+		pthread_join(thread_list_head->thread, NULL);
+		ThreadNode *temp = thread_list_head;
+		thread_list_head = thread_list_head->next;
+		free(temp);
+	}
+
+	pthread_mutex_destroy(&file_mutex);
+	pthread_mutex_destroy(&list_mutex);
+	closelog();
+
 	return 0;
 }
